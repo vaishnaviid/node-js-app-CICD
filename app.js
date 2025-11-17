272d2c7d9ccf899d1a2ed58c24f850c5ec49d44a
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-11-17 19:48:14 +0530

    new change in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from jenkins, added webhook, deployed sucessfully');
+  res.send('node app with github webhook deployed sucessfully');
 });
 
 app.listen(port, () => {
